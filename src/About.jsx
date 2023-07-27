a57f018cf78c474fee527809546b2d6c7862aa32
ImranKhan071 <im2002khan@gmail.com>
2023-07-27 17:29:55 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This Blog App is a Project Build Using ReactJs. It can be Used as a Personal Blog
+      </p>
+    </main>
+  );
+};
+
+export default About;
